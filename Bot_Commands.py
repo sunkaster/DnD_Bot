5b138ef0cadb73d9e3c4f753af94fab6bcc5280e
Sunkaster <54435371+sunkaster@users.noreply.github.com>
2025-07-27 21:39:35 +0200

    awjeff

--- Bot_Commands.py
+++ Bot_Commands.py
@@ -1,6 +1,7 @@
 HELPSTRING='''- !Help: Displays command list.
 - !Hello: Returns "Hello!".
 - !Roll: Used to roll dice in 1d10 format. Use "+" to add dice total togheter and "," to count them separately. To make multiple rolls of the same type easier use "(1d20)x10" for example to make 10 seperate d20 rolls
+- !CC: (Not implemented)If left empty will return Rolled values and total scores for Character creation. You can also enter abilities (!CC cha,str,int) in order from which should get the highest value rolled and the values will set themselves.
 '''
 
 COMMANDS_LIST={
